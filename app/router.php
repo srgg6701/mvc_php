6c68314202545ddcf9210b717ed743ea4d1645b4
srgg6701 <srgg67@gmail.com>
2014-08-19 19:16:33 +0400

    Добавление обработчика промежуточных сегментов URL.

--- app/router.php
+++ app/router.php
@@ -3,9 +3,11 @@
 $droot = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR;
 $key_site_name = array_search(SITE_NAME,$arr_uri); //echo "key_site_name = $key_site_name"; die();
 if($key_site_name>1){
+    $addUrlSegments=array(); // то, что будем добавлять к Url, если между HTTP_HOST и именем сайта есть ещё что-то
     $cnt=$key_site_name-1;
     while($cnt){
         $droot.= $arr_uri[$cnt] . DIRECTORY_SEPARATOR;
+        $addUrlSegments[]=$arr_uri[$cnt];
         $cnt--;
     }
 }
@@ -17,28 +19,26 @@ foreach ($url_segments as $i=>$segment) {
     if(strstr($segment,"?")||strstr($segment,"&")){
         $sep=strstr($segment,"?")? "?":"&";
         $s=explode($sep,$segment);
-        unset($s[1]);
-        $url_segments[$i]=$s[0];
-        if($url_segments[$i]=='')
+        unset($s[1]); // удалить всё после разделителя параметров
+        $url_segments[$i]=$s[0]; // оставить то, что осталось слева (имеет смысл, если перед разделителем не было слэша)
+        if($url_segments[$i]=='') // если осталась пустая строка (был слэш) - удалить элемент
             unset($url_segments[$i]);
     }
 } // echo "<pre>"; var_dump($url_segments); echo "</pre>";
+// создадим статический путь подключения файлов по http
 $static_path='http://'.$_SERVER['SERVER_NAME'] ;
 /*  echo "<pre>"; var_dump($_SERVER); echo "</pre>"; //die();
     echo('static_path = ' . $static_path . "<hr>"); //http://127.0.0.1/
     die('HOST_NAME = ' . $_SERVER['HOST_NAME']); */
+// если в адресе есть порт, добавим его
 if(strstr($_SERVER['HTTP_HOST'], ':'.$_SERVER['SERVER_PORT']))
     $static_path.= ':' . $_SERVER['SERVER_PORT'] . '/';
 $static_path.='/';
-
-if(strstr($_SERVER['REQUEST_URI'], '/projects/'))
-    $static_path.='projects/';
-
+if(isset($addUrlSegments))
+    $static_path.=implode("/",$addUrlSegments).'/';
+// добавим к статическому адресу имя сайта
 $static_path.= SITE_NAME . '/static/';
-// authors/         - список
-// authors/[id]     - данные сущности по id
-// authors/create   - форма добавления сущности
-//echo "<pre>"; var_dump($url_segments); echo "</pre>";
+// переходим к сути
 $entity=NULL;
 if(!empty($url_segments)
     && isset($url_segments[0])){
