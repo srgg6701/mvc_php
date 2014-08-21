b6e1dced477e6feca8f28f44546b70d1dc271a6e
srgg6701 <srgg67@gmail.com>
2014-08-21 22:46:15 +0400

    Косметическая модификация вывода в профилировании.

--- app/views/AuthorsView.php
+++ app/views/AuthorsView.php
@@ -4,6 +4,7 @@ class AuthorsView {
             $html =showMethodName(__METHOD__);
             $html.=$modelData;
             $html.="<div>HTML coming from view after \$modelData being handled...</div>";
+            $html="<div class='info'>" . $html . "</div>";
         return $html;
     }
 } 
